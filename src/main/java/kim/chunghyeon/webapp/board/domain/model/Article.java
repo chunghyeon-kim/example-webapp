5262c2230571a839f19c0be0e77ca4b8aa75fd49
chunghyeon-kim <asdddq11@gmail.com>
2021-01-16 16:15:34 +0900

    [#1] add spring-data and set dialect MySQL

--- src/main/java/kim/chunghyeon/webapp/board/domain/model/Article.java
+++ src/main/java/kim/chunghyeon/webapp/board/domain/model/Article.java
@@ -3,10 +3,14 @@ package kim.chunghyeon.webapp.board.domain.model;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.ZonedDateTime;
 
 @Getter
+@Entity
 public class Article {
+    @Id
     private Long id;
     private String subject;
     private String content;
