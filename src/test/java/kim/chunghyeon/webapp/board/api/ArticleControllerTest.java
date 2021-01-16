fb3bbf5915bb91c38fc0d4032c5ab2c594a8ceb6
chunghyeon-kim <asdddq11@gmail.com>
2021-01-16 19:38:27 +0900

    [#3] add ControllerExceptionAdvice

--- src/test/java/kim/chunghyeon/webapp/board/api/ArticleControllerTest.java
+++ src/test/java/kim/chunghyeon/webapp/board/api/ArticleControllerTest.java
@@ -3,6 +3,7 @@ package kim.chunghyeon.webapp.board.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kim.chunghyeon.webapp.board.api.dto.ArticleRequestDto;
 import kim.chunghyeon.webapp.board.api.dto.ArticleRequestDtoFixture;
+import kim.chunghyeon.webapp.board.domain.exception.ArticleNotFoundException;
 import kim.chunghyeon.webapp.board.domain.service.ArticleService;
 import kim.chunghyeon.webapp.common.api.dto.CreationResponseDto;
 import org.junit.jupiter.api.Test;
@@ -12,15 +13,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -63,5 +65,17 @@ class ArticleControllerTest {
                                 fieldWithPath("id").description("생성한 아티클 ID")
                         )
                 ));
+    }
+
+    @Test
+    void get_nonExistentArticle_notFound() throws Exception {
+        given(articleService.getArticle(anyLong())).willThrow(new ArticleNotFoundException(1L));
+
+        mockMvc.perform(get("/articles/{id}", 1))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.timestamp").isString())
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.error").value("Not Found"))
+                .andExpect(jsonPath("$.message").value("article #1 has not been found."));
     }
 }
