c17052c3973dcf975b50d3e8213333ffefd46893
chunghyeon-kim <asdddq11@gmail.com>
2021-01-16 16:53:57 +0900

    [#1] add ArticleRepository and its test.

--- src/main/java/kim/chunghyeon/webapp/board/domain/model/Article.java
+++ src/main/java/kim/chunghyeon/webapp/board/domain/model/Article.java
@@ -2,24 +2,30 @@ package kim.chunghyeon.webapp.board.domain.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.ZonedDateTime;
 
 @Getter
 @Entity
+@NoArgsConstructor
 public class Article {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String subject;
     private String content;
 
+    @CreationTimestamp
     private ZonedDateTime createdAt;
+    @UpdateTimestamp
     private ZonedDateTime updatedAt;
 
     @Builder
-    public Article(String subject, String content) {
+    private Article(String subject, String content) {
         this.subject = subject;
         this.content = content;
     }
---
+++ src/test/java/kim/chunghyeon/webapp/board/domain/model/ArticleRepositoryTest.java
@@ -0,0 +1,43 @@
+package kim.chunghyeon.webapp.board.domain.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
+
+@DataJpaTest
+class ArticleRepositoryTest {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Test
+    void save_validInput_validOutput() {
+        // given
+        Article article = Article.builder().subject("subject").content("content").build();
+        assertThat(article.getId()).isNull();
+
+        // when
+        Article savedArticle = articleRepository.save(article);
+
+        // then
+        then(article.getId()).isNotNull();
+        then(article.getCreatedAt()).isNotNull();
+        then(article.getUpdatedAt()).isNotNull();
+        then(article == savedArticle).isTrue();
+    }
+
+    @Test
+    @Sql("/data/test-article-data.sql")
+    void findAllByIdDesc_validInput_validOutput() {
+        List<Article> articles = articleRepository.findAllByOrderByIdDesc();
+
+        then(articles).extracting("id").containsExactly(2L, 1L);
+    }
+
+}
